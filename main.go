ef47a2c0d13e146ecabb849d8358da6d822aeb8c
Ilia Choly <ilia.choly@gmail.com>
2024-03-23 18:48:23 -0400

    create main file

--- main.go
+++ main.go
@@ -1,101 +1,38 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"os"
-	"path/filepath"
-	"slices"
+	"bytes"
+	"flag"
+	"log"
+	"os/exec"
+	"strings"
 )
 
-type Pos struct {
-	Col    int `json:"col"`
-	Line   int `json:"line"`
-	Offset int `json:"offset"`
-}
-
-type Result struct {
-	CheckID string `json:"check_id"`
-	End     Pos    `json:"end"`
-	Extra   Extra  `json:"extra"`
-	Path    string `json:"path"`
-	Start   Pos    `json:"start"`
-}
-
-type Extra struct {
-	EngineKind  string         `json:"engine_kind"`
-	Fingerprint string         `json:"fingerprint"`
-	IsIgnored   bool           `json:"is_ignored"`
-	Lines       string         `json:"lines"`
-	Message     string         `json:"message"`
-	Metadata    map[string]any `json:"metadata"`
-	Severity    string         `json:"severity"`
-}
-
-type Paths struct {
-	Comment string   `json:"_comment"`
-	Scanned []string `json:"scanned"`
-}
-
-type Output struct {
-	Errors  []any    `json:"errors"`
-	Paths   Paths    `json:"paths"`
-	Results []Result `json:"results"`
-	Version string   `json:"version"`
-}
-
-// ReadFile reads and parses a file containing semgrep json
-func ReadFile(name string) (*Output, error) {
-	data, err := os.ReadFile(name)
+func main() {
+	// parse flags
+	var file, dir string
+	flag.StringVar(&file, "f", "semgrep.json", "semgrep json file")
+	flag.StringVar(&dir, "d", ".", "directory to run in")
+	flag.Parse()
+	if flag.NArg() == 0 {
+		log.Fatalf("expecting a command to run")
+	}
+	// read semgrep file
+	output, err := ReadFile(file)
 	if err != nil {
-		return nil, err
+		log.Fatalf("failed to read semgrep json: %v", err)
 	}
-	var output Output
-	if err := json.Unmarshal(data, &output); err != nil {
-		return nil, err
-	}
-	return &output, nil
-}
-
-type RewriteFn = func(r Result) ([]byte, error)
-
-var ErrSkip = errors.New("skip")
-
-func Rewrite(data []byte, results []Result, rewrite RewriteFn) ([]byte, error) {
-	slices.SortFunc(results, func(a, b Result) int {
-		return b.Start.Offset - a.Start.Offset
-	})
-	for _, r := range results {
-		rewritten, err := rewrite(r)
-		if err == ErrSkip {
-			continue
-		}
-		if err != nil {
+	err = RewriteAll(dir, output.Results, func(r Result) ([]byte, error) {
+		var output bytes.Buffer
+		cmd := exec.Command(flag.Arg(0), flag.Args()[1:]...)
+		cmd.Stdin = strings.NewReader(r.Extra.Lines)
+		cmd.Stdout = &output
+		if err := cmd.Run(); err != nil {
 			return nil, err
 		}
-		data = slices.Replace(data, r.Start.Offset, r.End.Offset, rewritten...)
+		return output.Bytes(), nil
+	})
+	if err != nil {
+		log.Fatalf("failed to rewrite: %v", err)
 	}
-	return data, nil
-}
-
-func RewriteAll(dir string, results []Result, rewrite RewriteFn) error {
-	files := map[string][]Result{}
-	for _, r := range results {
-		files[r.Path] = append(files[r.Path], r)
-	}
-	for file, rr := range files {
-		path := filepath.Join(dir, file)
-		data, err := os.ReadFile(path)
-		if err != nil {
-			return err
-		}
-		data, err = Rewrite(data, rr, rewrite)
-		if err != nil {
-			return err
-		}
-		if err := os.WriteFile(path, data, os.ModePerm); err != nil {
-			return err
-		}
-	}
-	return nil
 }
---
+++ semgrep.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+	"slices"
+)
+
+type Pos struct {
+	Col    int `json:"col"`
+	Line   int `json:"line"`
+	Offset int `json:"offset"`
+}
+
+type Result struct {
+	CheckID string `json:"check_id"`
+	End     Pos    `json:"end"`
+	Extra   Extra  `json:"extra"`
+	Path    string `json:"path"`
+	Start   Pos    `json:"start"`
+}
+
+type Extra struct {
+	EngineKind  string         `json:"engine_kind"`
+	Fingerprint string         `json:"fingerprint"`
+	IsIgnored   bool           `json:"is_ignored"`
+	Lines       string         `json:"lines"`
+	Message     string         `json:"message"`
+	Metadata    map[string]any `json:"metadata"`
+	Severity    string         `json:"severity"`
+}
+
+type Paths struct {
+	Comment string   `json:"_comment"`
+	Scanned []string `json:"scanned"`
+}
+
+type Output struct {
+	Errors  []any    `json:"errors"`
+	Paths   Paths    `json:"paths"`
+	Results []Result `json:"results"`
+	Version string   `json:"version"`
+}
+
+// ReadFile reads and parses a file containing semgrep json
+func ReadFile(name string) (*Output, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	var output Output
+	if err := json.Unmarshal(data, &output); err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
+
+type RewriteFn = func(r Result) ([]byte, error)
+
+var ErrSkip = errors.New("skip")
+
+func Rewrite(data []byte, results []Result, rewrite RewriteFn) ([]byte, error) {
+	slices.SortFunc(results, func(a, b Result) int {
+		return b.Start.Offset - a.Start.Offset
+	})
+	for _, r := range results {
+		rewritten, err := rewrite(r)
+		if err == ErrSkip {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+		data = slices.Replace(data, r.Start.Offset, r.End.Offset, rewritten...)
+	}
+	return data, nil
+}
+
+func RewriteAll(dir string, results []Result, rewrite RewriteFn) error {
+	files := map[string][]Result{}
+	for _, r := range results {
+		files[r.Path] = append(files[r.Path], r)
+	}
+	for file, rr := range files {
+		path := filepath.Join(dir, file)
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		data, err = Rewrite(data, rr, rewrite)
+		if err != nil {
+			return err
+		}
+		if err := os.WriteFile(path, data, os.ModePerm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
