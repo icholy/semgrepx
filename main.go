8f7d7be46fe9d82909ac70842c8681901592d50d
Ilia Choly <ilia.choly@gmail.com>
2024-03-23 20:07:22 -0400

    add -trim

--- main.go
+++ main.go
@@ -15,8 +15,10 @@ import (
 func main() {
 	// parse flags
 	var file, dir string
+	var trim bool
 	flag.StringVar(&file, "f", "semgrep.json", "semgrep json file")
 	flag.StringVar(&dir, "d", ".", "directory to run in")
+	flag.BoolVar(&trim, "t", false, "trim whitespace")
 	flag.Parse()
 	if flag.NArg() == 0 {
 		log.Fatalf("expecting a command to run")
@@ -31,19 +33,23 @@ func main() {
 			r.Path,
 			FormatLines(string(data), r.Start.Line, 5),
 		)
-		var output bytes.Buffer
+		var stdout bytes.Buffer
 		cmd := exec.Command(flag.Arg(0), flag.Args()[1:]...)
 		cmd.Stdin = bytes.NewReader(data)
-		cmd.Stdout = &output
+		cmd.Stdout = &stdout
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
 			return nil, err
 		}
+		output := stdout.Bytes()
+		if trim {
+			output = bytes.TrimSpace(output)
+		}
 		fmt.Printf("--- after: %s\n%s\n",
 			r.Path,
-			FormatLines(output.String(), r.Start.Line, 5),
+			FormatLines(string(output), r.Start.Line, 5),
 		)
-		return output.Bytes(), nil
+		return output, nil
 	})
 	if err != nil {
 		log.Fatalf("failed to rewrite: %v", err)
